56702f08684dfcd471e9acb8e48e1527250434e1
Paweł Żurawka <pzurawka@gmail.com>
2019-06-12 17:56:17 +0200

    Task done

--- app/program.js
+++ app/program.js
@@ -1,23 +1,23 @@
-var OSinfo = require('../modules/OSinfo');
+const OSinfo = require('../modules/OSinfo');
 
 process.stdin.setEncoding('utf-8');
-process.stdin.on('readable', function() {
-    var input = process.stdin.read();
+process.stdin.on('readable', () => {
+    const input = process.stdin.read();
     if(input !== null) {
         var instruction = input.toString().trim();
         switch(instruction) {
             case '/exit':
-                process.stdout.write('Quitting app!\n');
+                process.stdout.write(`Quitting app!\n`);
                 process.exit();
                 break;
             case '/info':
-                process.stdout.write('Node version: ' + process.versions.node + '\n' + 'System language and encoding: ' + process.env.LANG + '\n');
+                process.stdout.write(`Node version: ${process.versions.node}\nSystem language and encoding: ${process.env.LANG}\n`);
                 break;
             case '/getOSinfo':
                 OSinfo.print();
                 break;
             default:
-                process.stderr.write('Wrong instruction!\n');
+                process.stderr.write(`Wrong instruction!\n`);
         }
     }
 });
